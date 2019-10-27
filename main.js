bf8cff085ae6eb272f2fa1364a2187ea388119b5
Joe Murphy <jmurphy2591@gmail.com>
2019-10-27 15:16:50 -0400

    v1 updates

---
+++ main.js
@@ -0,0 +1,19 @@
+function sentSuccess() {
+    let firstName = document.getElementById("first_name");
+    let phone = document.getElementById("phone_number");
+    let email = document.getElementById("email");
+    let subject = document.getElementById("subject");
+    let message = document.getElementById("message");
+
+    if (
+        firstName.value !== "" && 
+        phone.value !== "" &&
+        email.value !== "" &&
+        subject.value !== "" &&
+        message.value !== ""
+    ) {
+        let success = document.getElementById("success");
+        success.style.display = "inline-block";
+    }
+}
+
